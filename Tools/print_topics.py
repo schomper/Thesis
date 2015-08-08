83907e92ad0fc702717e21ae2c931f395c812865
schomper <delan.schomper@gmail.com>
2015-08-08 18:44:00 +1000

    Converted print_topics to python 3

--- Tools/print_topics.py
+++ Tools/print_topics.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 # usage: python print_topics.py <beta file> <vocab file> <num words>
 #
@@ -14,29 +14,30 @@ OUTPUT_FILE_NAME = "../Output/topic_results.out"
 def print_topics(beta_file, vocab_file, nwords=25):
     # get the vocabulary
 
-    vocab = file(vocab_file, 'r').readlines()
-    # vocab = map(lambda x: x.split()[0], vocab)
-    vocab = map(lambda x: x.strip(), vocab)
+    vocab = open(vocab_file, 'r').readlines()
+    vocab = list(map(lambda x: x.strip(), vocab))
 
     # for each line in the beta file
-
-    indices = range(len(vocab))
+    indices = list(range(len(vocab)))
     topic_no = 0
 
     file_pointer = open(OUTPUT_FILE_NAME, "w")
 
-    for topic in file(beta_file, 'r'):
+    for topic in open(beta_file, 'r'):
         file_pointer.write('Topic %03d' % topic_no)
 
-        print 'Topic %03d' % topic_no
-        topic = map(float, topic.split())
-        indices.sort(lambda x, y: -cmp(topic[x], topic[y]))
+        print('Topic %03d' % topic_no)
+        topic = list(map(float, topic.split()))
+        indices = sorted(indices, key=lambda x: -topic[x])
+        #indices.sort(lambda x, y: -cmp(topic[x], topic[y]))
+
         for i in range(nwords):
             file_pointer.write('   %s\n' % vocab[indices[i]])
-            print '   %s' % vocab[indices[i]]
+            print('   %s' % vocab[indices[i]])
+        
         topic_no += 1
         file_pointer.write("\n");
-        print '\n'
+        print('\n')
 
     file_pointer.close();
 
@@ -44,7 +45,7 @@ def print_topics(beta_file, vocab_file, nwords=25):
 if __name__ == '__main__':
 
     if len(sys.argv) != 4:
-        print 'usage: python topics.py <beta-file> <vocab-file> <num words>\n'
+        print('usage: python topics.py <beta-file> <vocab-file> <num words>\n')
         sys.exit(1)
 
     beta_file = sys.argv[1]
